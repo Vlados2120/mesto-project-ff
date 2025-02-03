0779931a699a903928ff699087382e8c0b432e41
Vlad Sysoev <sysoyev.04@list.ru>
2025-02-03 14:04:41 +0300

    Место

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
 
-// @todo: DOM узлы
+function createCard(cardData, deleteCallback) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-// @todo: Функция создания карточки
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  deleteButton.addEventListener("click", () => deleteCallback(cardElement));
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+function renderCards() {
+  initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.appendChild(cardElement);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", renderCards);
